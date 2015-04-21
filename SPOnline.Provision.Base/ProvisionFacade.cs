c724ccd0a6c1ae4b62f8a31ef765f584b4c0fe03
kaulshivank <kaulshivank@gmail.com>
2015-04-21 10:08:34 +0800

    Base

---
+++ SPOnline.Provision.Base/ProvisionFacade.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SPOnline.Provision.Base
+{
+    public class ProvisionFacade
+    {
+    }
+}
